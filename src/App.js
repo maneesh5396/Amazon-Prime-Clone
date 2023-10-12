a53eee03111dce7610671f66d131564c8a572c0f
Maneesh Prajapati <manishkp16@gmail.com>
2023-10-12 17:49:32 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ResponsiveAppBar from './component/Navbar/Navbar';
+// import '../styles/App.css';
+// import {Routes, Route} from 'react-router-dom';
+// import NavBar from './component/Navbar/Navbar';
 
-function App() {
+// import logo from "../src/assets/prime-logo.png";
+// export function Logo(){
+//     return <img className="logo" alt="amazon-prime-logo" src={logo} />
+// }
+// export default Logo;
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ResponsiveAppBar />
+      {/* <NavBar /> */}
+      {/* <Logo /> */}
+      {/* <Routes>
+        <Route>Home</Route>
+        <Route>Home</Route>
+      </Routes> */}
+    </>
+  )
 }
+
 
 export default App;
